246321b7f5ac7aaf9c4f93e1c8a068a45528212d
jiwonshin84 <jiwonshin84@gmail.com>
2026-08-31 17:56:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,24 @@ st.set_page_config(
 pages = {
     "메인": [
         st.Page("pages/1_실시간 대시보드.py", title="실시간 대시보드", icon="🏠"),
-        st.Page("pages/2_프로젝트 개요.py", title="프로젝트 개요", icon="📋"),
+        st.Page("pages/2_프로젝트 개요.py", title="프로젝트 개요", icon="📖"),
     ],
     "문화유산": [
-        st.Page("pages/3_전국문화유산현황.py", title="전국 문화유산 현황", icon="🤖"),
-        #st.Page("pages/4_ai_explain.py", title="영천 국가유산 공간 정", icon="🧠"),
+        st.Page("pages/3_전국문화유산현황.py", title="전국 문화유산 현황", icon="🏛"),
+        st.Page("pages/4_영천 국가유산 공간 정보.py", title="영천 국가유산 공간 정보", icon="🗺"),     
+        st.Page("pages/5_AI 문화재 해설.py", title="AI 문화재 해설", icon="🧠"),     
+    ],
+    "수집&학습&예측": [
+        st.Page("pages/6_학습 데이터 수집.py", title="학습 데이터 수집", icon="🚀"),
+        st.Page("pages/7_위험 예측 분류 모델 학습.py", title="위험 예측 분류 모델 학습", icon="🗂️"),     
+        st.Page("pages/7_위험 예측 분류 모델 학습 최적화.py", title="위험 예측 분류 모델 학습 최적화", icon="🗂️"),             
+        st.Page("pages/8_전일~7일전 데이터.py", title="전일~7일전 데이터", icon="🔍"),  
+        st.Page("pages/8_예측.py", title="예측", icon="💡"),     
+    ],
+    "피지컬": [
+        st.Page("pages/9_피코 실시간 데이터.py", title="피코 실시간 데이터", icon="🌦"),
+        st.Page("pages/10_테스트.py", title="테스트", icon="🧪"),     
+        st.Page("pages/11_군집 분석.py", title="군집 분석", icon="🛡️")
     ]
 }
 
