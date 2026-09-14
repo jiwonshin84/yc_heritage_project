5c4bb72230767d1dd85203fafc47c1795563cab4
jiwonshin84 <jiwonshin84@gmail.com>
2026-09-14 18:04:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,26 +10,29 @@ st.set_page_config(
 # 사이드바 메뉴 및 연결할 실제 파일 경로 설정
 pages = {
     "메인": [
-        st.Page("pages/1_실시간 대시보드.py", title="실시간 대시보드", icon="🏠"),
-        st.Page("pages/2_프로젝트 개요.py", title="프로젝트 개요", icon="📖"),
+        st.Page("pages/01_실시간 대시보드.py", title="실시간 대시보드", icon="🏠"),
+        st.Page("pages/02_프로젝트 개요.py", title="프로젝트 개요", icon="📖"),
     ],
-    "문화유산": [
-        st.Page("pages/3_전국문화유산현황.py", title="전국 문화유산 현황", icon="🏛"),
-        st.Page("pages/4_영천 국가유산 공간 정보.py", title="영천 국가유산 공간 정보", icon="🗺"),     
-        st.Page("pages/5_AI 문화재 해설.py", title="AI 문화재 해설", icon="🧠"),     
+    "문화유산 탐색": [
+        st.Page("pages/03_전국문화유산현황.py", title="전국 문화유산 현황", icon="🏛"),
+        st.Page("pages/04_영천 국가유산 공간 정보.py", title="영천 국가유산 공간 정보", icon="🗺"),     
+        st.Page("pages/05_AI 문화재 해설.py", title="AI 문화재 해설", icon="🧠"),     
     ],
-    "수집&학습&예측": [
-        st.Page("pages/6_학습 데이터 수집.py", title="학습 데이터 수집", icon="🚀"),
-        st.Page("pages/7_위험 예측 분류 모델 학습.py", title="위험 예측 분류 모델 학습", icon="🗂️"),     
-        st.Page("pages/7_위험 예측 분류 모델 학습 최적화.py", title="위험 예측 분류 모델 학습 최적화", icon="🗂️"),             
-        st.Page("pages/8_전일~40일전 데이터.py", title="전일~40일전 데이터", icon="🔍"),  
-        st.Page("pages/8_예측.py", title="예측", icon="💡"),     
+    "환경 취약도 분석": [
+        st.Page("pages/06_학습 데이터 수집.py", title="학습 데이터 수집", icon="🚀"),
+        st.Page("pages/07_환경 취약도 분류 모델 학습.py", title="환경 취약도 분류 모델 학습", icon="🗂️"),     
+        st.Page("pages/08_모델 성능 및 최적화.py", title="모델 성능 및 최적화", icon="🗂️"),             
+        st.Page("pages/09_최근 40일 환경 데이터.py", title="최근 40일 환경 데이터", icon="🔍"),  
+        st.Page("pages/10_문화유산 취약도 예측.py", title="문화유산 취약도 예측", icon="💡"),     
     ],
-    "피지컬": [
-        st.Page("pages/9_피코 실시간 데이터.py", title="피코 실시간 데이터", icon="🌦"),
-        st.Page("pages/10_테스트.py", title="테스트", icon="🧪"),     
-        st.Page("pages/11_군집 분석.py", title="군집 분석", icon="🛡️"),
-        st.Page("pages/12_테스트2.py", title="테스트2", icon="🛡️"),
+    "실시간 모니터링": [
+        st.Page("pages/11_Pico 실시간 환경 데이터.py", title=" Pico 실시간 환경 데이터", icon="🌦"),
+    ],
+    
+    "부가 프로젝트": [
+        st.Page("pages/12_테스트.py", title="테스트", icon="🧪"),     
+        st.Page("pages/13_군집 분석.py", title="군집 분석", icon="🛡️"),
+        st.Page("pages/14_테스트2.py", title="테스트2", icon="🛡️"),
     ]
 }
 
